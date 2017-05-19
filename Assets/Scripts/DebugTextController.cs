a6a615454beaae23e6f2ddba4136ee2689ea8c38
有光喜一 <arimitu@ouk.jp>
2017-05-19 10:10:03 +0900

    とりあえず起動する所まで

---
+++ Assets/Scripts/DebugTextController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugTextController : MonoBehaviour {
+    public static string DebugText_;
+
+    Text Text_;
+
+    // Use this for initialization
+    void Start() {
+        Text_ = GetComponent<Text>();
+        DebugText_ = "debug";
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Text_.text = DebugText_;
+    }
+}
